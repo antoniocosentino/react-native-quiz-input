b1ffa194b08ea9a3678505d22f534b0b7fc550b1
Antonio Cosentino <hello@antoniocosentino.it>
2021-04-22 22:23:33 +0200

    removed original sample app

--- example/index.js
+++ example/index.js
@@ -5,7 +5,6 @@
 import { AppRegistry } from 'react-native';
 
 import { name as appName } from './app.json';
-//import App from './src/App'
 import App from './storybook';
 
 AppRegistry.registerComponent( appName, () => App );
